1aa2a9e5fc0d58c8fde040cb942b91e045125049
Captain Allen <69441493+mrallen-m@users.noreply.github.com>
2022-12-30 15:07:16 -0500

    additions

--- src/blockchain.js
+++ src/blockchain.js
@@ -4,7 +4,7 @@ const { v1: uuidv1 } = require('uuid');
 
 function Blockchain() {
     this.chain = []; // this property is the meat of our chain will be stored. all of the blocks we create will be stored in this array as a chain.
-    this.newTransactions = []; // holds all of the new transactions before they are placed into the block and mined.
+    this.pendingTransactions = []; // holds all of the new transactions before they are placed into the block and mined.
     this.transaction = [];
 
     this.currentNodeUrl = currentNodeUrl;
@@ -20,13 +20,13 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1, // the block number
         timestamp: Date.now(), // gets the date
-        transactions: this.newTransactions, // gets all of the new transactions
+        transactions: this.pendingTransactions, // gets all of the new transactions
         nonce: nonce, // number with the POW method that proves legitimacy
         hash: hash, // the data from the current block...gets compressed into a single string
         previousBlockHash: previousBlockHash // data from previous block
     };
 
-    this.newTransactions = []; // clears out this array to start over for the next block
+    this.pendingTransactions = []; // clears out this array to start over for the next block
     this.chain.push(newBlock); // pushes data to the chain
 
     return newBlock;
@@ -84,19 +84,19 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
 
     
 
-    //push into newTransaction into newTransactions array
-    // this.newTransactions.push(newTransaction);
+    //push into newTransaction into pendingTransactions array
+    // this.pendingTransactions.push(newTransaction);
 
     // return the # of the block that this transaction will be added to
     // return this.getLastBlock()['index'] + 1;
 
 
-Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj) {
-    this.newTransactions.push(transactionObj);
+Blockchain.prototype.addTransactionToNewTransactions = function(transactionObj) {
+    this.pendingTransactions.push(transactionObj);
     return this.getLastBlock()['index'] + 1;
 }
 
-// Use SHA256. Need to install.
+
 // Takes in block data and hashes the string. Pass data of a block into the method and will return hash
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
     // need to turn data into a string
--- src/test.js
+++ src/test.js
@@ -2,8 +2,57 @@ const Blockchain = require('./blockchain');
 
 const misterchain = new Blockchain();
 
+
+
+// TESTS CREATENEWBLOCK METHOD
 misterchain.createNewBlock(2389, 'OINA90SDNF90N', '90ANSD9F0N9009N');
 misterchain.createNewBlock(111, 'OIANSDF0AN09', 'NJNASDNF09ASDF');
 misterchain.createNewBlock(2389, 'UINIUN90ANSDF', '99889HBAIUSBDF');
 
 console.log(misterchain);
+
+// TESTS CREATENEWTRANSACTION METHOD
+misterchain.createNewBlock(123456, 'ODIHADIOPH78', 'DHNWBK8YEV');
+misterchain.createNewTransaction(200, 'SDFHJHWEBJR873HHGE6', '37YH2JGEW92HJ');
+misterchain.createNewBlock(123456, 'RWEGU37HERJH35', '8RUWJ92G1NHE7');
+
+console.log(misterchain.chain[1]);
+
+// TESTS CREATENEWTRANSACTION METHOD with 3 new pending transactions
+misterchain.createNewBlock(123456, 'ODIHADIOPH78', 'DHNWBK8YEV');
+misterchain.createNewTransaction(200, 'SDFHJHWEBJR873HHGE6', '37YH2JGEW92HJ');
+misterchain.createNewBlock(123456, 'RWEGU37HERJH35', '8RUWJ92G1NHE7');
+
+misterchain.createNewTransaction(100, 'SDFHJHWEBJR873HHGE6', '37YH2JGEW92HJ');
+misterchain.createNewTransaction(200, 'SDFHJHWEBJR873HHGE6', '37YH2JGEW92HJ');
+misterchain.createNewTransaction(300, 'SDFHJHWEBJR873HHGE6', '37YH2JGEW92HJ');
+
+misterchain.createNewBlock(123456, '7YEHVWVNX9UKW', '09YW6O2KNSMKE0');
+
+console.log(misterchain.chain[2]);
+
+
+// TESTS THE HASHBLOCK FUNCTION
+const previousBlockHash = 'OUEHRW7823GUY23GU6ERWJEHRV803';
+const currentBlockData = [
+    {
+        amount: 100,
+        sender: 'IUWEYIRGUE823YIEJH32',
+        recipient: '90IUEBRW73YU2V381Y348'
+    },
+    {
+        amount: 100,
+        sender: 'IUWEYIRGUE823YIEJH32',
+        recipient: '90IUEBRW73YU2V381Y348'
+    },
+    {
+        amount: 100,
+        sender: 'IUWEYIRGUE823YIEJH32',
+        recipient: '90IUEBRW73YU2V381Y348'
+    }
+];
+const nonce = 100;
+
+console.log(misterchain.hashBlock(previousBlockHash, currentBlockData, nonce));
+
+
