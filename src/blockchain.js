6069f438b78d0d93892544de7a4f9f679c53657c
Captain Allen <69441493+mrallen-m@users.noreply.github.com>
2023-01-02 18:12:14 -0500

    html

--- src/blockchain.js
+++ src/blockchain.js
@@ -51,46 +51,6 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
     return newTransaction;
 }
 
-// class Transaction {
-//     // validate signature
-//     //will return the 256hash
-//     calculateHash() {
-//         return sha256(this.sender + this.recipient + this.amount).toString();
-//     };
-
-//     signTransaction(signingKey) {
-
-//         if(signingKey.getPublic('hex') !== this.fromAddress) {
-//             throw newError('You cannot sign transactions for other wallets!');
-
-//         }
-//         const hashTX = this.calculateHash();
-//         const sig = signingKey.sign(hashTx, 'base64');
-//         this.signature = sig.toDER('hex');
-//     }
-
-//     isValid(){
-//         if(this.sender === null) return true;
-
-//         if(!this.signature || this.signature.length === 0) {
-//             throw new Error('No signature in this transaction');
-//         }
-
-//         const publicKey = ec.keyFromPublic(this.sender, 'hex');
-//         return publicKey.verify(this.calculateHash(), this.signature);
-//     }
-
-// }   
-
-    
-
-    //push into newTransaction into pendingTransactions array
-    // this.pendingTransactions.push(newTransaction);
-
-    // return the # of the block that this transaction will be added to
-    // return this.getLastBlock()['index'] + 1;
-
-
 Blockchain.prototype.addTransactionToNewTransactions = function(transactionObj) {
     this.pendingTransactions.push(transactionObj);
     return this.getLastBlock()['index'] + 1;
